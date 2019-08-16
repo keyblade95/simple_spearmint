f0f6257e111bf75eac6053a0ed9f6ca38800d8fc
Federico Parroni <Keyblade95@live.it>
2019-08-16 09:37:09 +0200

    Updated setup

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(
-    name='simple_spearmint',
-    version='0.0.0',
-    description='Thin wrapper class around spearmint',
-    author='Colin Raffel',
-    author_email='craffel@gmail.com',
-    url='https://github.com/craffel/simple_spearmint',
-    packages=['simple_spearmint'],
+    name='simple_spearmint_v2',
+    version='1.0.0',
+    description='Thin and improved wrapper class around spearmint',
+    author='Federico Parroni',
+    author_email='federicoparroni.95@gmail.com',
+    url='https://github.com/keyblade95/simple_spearmint',
+    packages=['simple_spearmint_v2'],
     long_description=long_description,
     classifiers=[
         "License :: OSI Approved :: MIT License",
@@ -18,7 +18,7 @@ setup(
         'Development Status :: 5 - Production/Stable',
         "Intended Audience :: Developers"
     ],
-    keywords='hyperparameter optimization',
+    keywords='bayesian hyperparameter optimization',
     license='MIT',
     install_requires=[
         'spearmint',
